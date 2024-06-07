fca4ab3bed314a83c1c3a552f89aacf8e7bc047d
Aleksey Imuzov <github@aimuzov.online>
2024-06-07 08:31:27 +0400

    refactor(ui): remove indents for statusline config

--- lua/lazyvimx/extras/ui/panels/status-line.lua
+++ lua/lazyvimx/extras/ui/panels/status-line.lua
@@ -19,50 +19,48 @@ local mode_icon_map = {
 }
 
 return {
-	{
-		"nvim-lualine/lualine.nvim",
+	"nvim-lualine/lualine.nvim",
 
-		opts = function(_, opts)
-			table.insert(opts.extensions, "fugitive")
-			table.insert(opts.extensions, "mason")
+	opts = function(_, opts)
+		table.insert(opts.extensions, "fugitive")
+		table.insert(opts.extensions, "mason")
 
-			opts.options.component_separators = { left = "", right = "" }
-			opts.options.section_separators = { left = "", right = "" }
-			opts.options.disabled_filetypes.statusline = {}
+		opts.options.component_separators = { left = "", right = "" }
+		opts.options.section_separators = { left = "", right = "" }
+		opts.options.disabled_filetypes.statusline = {}
 
-			opts.sections.lualine_a = {
-				{
-					function()
-						local mode_name_upper = require("lualine.utils.mode").get_mode()
-						local mode_name_lower = mode_name_upper:lower()
-						local mode_icon_raw = mode_icon_map[mode_name_upper]
-						local mode_icon_safety = mode_icon_raw == nil and "" or mode_icon_raw
+		opts.sections.lualine_a = {
+			{
+				function()
+					local mode_name_upper = require("lualine.utils.mode").get_mode()
+					local mode_name_lower = mode_name_upper:lower()
+					local mode_icon_raw = mode_icon_map[mode_name_upper]
+					local mode_icon_safety = mode_icon_raw == nil and "" or mode_icon_raw
 
-						return mode_icon_safety .. " " .. mode_name_lower
-					end,
-				},
-			}
+					return mode_icon_safety .. " " .. mode_name_lower
+				end,
+			},
+		}
 
-			opts.sections.lualine_b = { { "branch", icon = "" } }
+		opts.sections.lualine_b = { { "branch", icon = "" } }
 
-			opts.sections.lualine_y = {
-				{ "location", padding = { left = 1, right = 1 } },
-				{
-					function()
-						local current_line = vim.fn.line(".")
-						local total_lines = vim.fn.line("$")
-						local chars = { "", "󰪞", "󰪟", "󰪠", "󰪡", "󰪢", "󰪣", "󰪤", "󰪥" }
-						local line_ratio = current_line / total_lines
-						local index = math.ceil(line_ratio * #chars)
+		opts.sections.lualine_y = {
+			{ "location", padding = { left = 1, right = 1 } },
+			{
+				function()
+					local current_line = vim.fn.line(".")
+					local total_lines = vim.fn.line("$")
+					local chars = { "", "󰪞", "󰪟", "󰪠", "󰪡", "󰪢", "󰪣", "󰪤", "󰪥" }
+					local line_ratio = current_line / total_lines
+					local index = math.ceil(line_ratio * #chars)
 
-						return chars[index]
-					end,
-					separator = " ",
-					padding = { left = 1, right = 1 },
-				},
-			}
+					return chars[index]
+				end,
+				separator = " ",
+				padding = { left = 1, right = 1 },
+			},
+		}
 
-			opts.sections.lualine_z = {}
-		end,
-	},
+		opts.sections.lualine_z = {}
+	end,
 }
