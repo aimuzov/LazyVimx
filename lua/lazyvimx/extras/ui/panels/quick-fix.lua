90718f2846cd3a90acd59301ecff067f2aae0e9a
Aleksey Imuzov <github@aimuzov.online>
2024-06-10 21:20:23 +0400

    feat(ui): replace default quickfix by trouble

--- lua/lazyvimx/extras/ui/panels/quick-fix.lua
+++ lua/lazyvimx/extras/ui/panels/quick-fix.lua
@@ -1,1 +1,28 @@
-return {}
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		opts = function(_, opts)
+			local actions = require("telescope.actions")
+			local mappings = {
+				["<C-q>"] = actions.send_to_qflist,
+				["<M-q>"] = actions.send_selected_to_qflist,
+			}
+
+			opts.defaults.mappings = {
+				i = mappings,
+				n = mappings,
+			}
+		end,
+	},
+
+	{
+		"folke/trouble.nvim",
+		opts = function()
+			vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+				callback = function()
+					vim.cmd([[Trouble qflist open]])
+				end,
+			})
+		end,
+	},
+}
