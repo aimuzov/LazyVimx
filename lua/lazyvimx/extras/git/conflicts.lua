70a49195cbc1f8b9b527e9a8521fdfda28516867
Aleksey Imuzov <github@aimuzov.online>
2024-06-18 20:19:51 +0400

    feat(git): add notification on conflict detected

--- lua/lazyvimx/extras/git/conflicts.lua
+++ lua/lazyvimx/extras/git/conflicts.lua
@@ -1,4 +1,14 @@
 return {
 	"akinsho/git-conflict.nvim",
-	opts = { disable_diagnostics = true },
+
+	opts = function(_, opts)
+		opts.disable_diagnostics = true
+
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "GitConflictDetected",
+			callback = function()
+				vim.notify("Conflict detected in '" .. vim.fn.expand("%:.") .. "'")
+			end,
+		})
+	end,
 }
