598172f1091c88572eaa1ed3b2c6b937ad089121
Aleksey Imuzov <github@aimuzov.online>
2024-07-01 08:34:08 +0300

    fix(keys): fix incorrect keymap for dap

--- lua/lazyvimx/extras/keys/dap/chrome-keys-style.lua
+++ lua/lazyvimx/extras/keys/dap/chrome-keys-style.lua
@@ -3,9 +3,9 @@ return {
 	optional = true,
 
 	keys = {
-		{ "n", "<F5>", [[<cmd>require("dap").continue()<cr>]], desc = "Continue" },
-		{ "n", "<F10>", [[<cmd>require("dap").step_over()<cr>]], desc = "Step over" },
-		{ "n", "<F11>", [[<cmd>require("dap").step_into()<cr>]], desc = "Step into" },
-		{ "n", "<F12>", [[<cmd>require("dap").step_out()<cr>]], desc = "Step out" },
+		{ "<F5>", [[<cmd>require("dap").continue()<cr>]], mode = { "n" }, desc = "Continue" },
+		{ "<F10>", [[<cmd>require("dap").step_over()<cr>]], mode = { "n" }, desc = "Step over" },
+		{ "<F11>", [[<cmd>require("dap").step_into()<cr>]], mode = { "n" }, desc = "Step into" },
+		{ "<F12>", [[<cmd>require("dap").step_out()<cr>]], mode = { "n" }, desc = "Step out" },
 	},
 }
