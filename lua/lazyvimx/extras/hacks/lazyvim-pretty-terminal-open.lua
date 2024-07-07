74f4a421ad1a6f33492ae5fae38f827141415603
Aleksey Imuzov <github@aimuzov.online>
2024-07-07 12:18:38 +0400

    fix(ui): fix backdrop for lazygit & remove bottom padding

--- lua/lazyvimx/extras/hacks/lazyvim-pretty-terminal-open.lua
+++ lua/lazyvimx/extras/hacks/lazyvim-pretty-terminal-open.lua
@@ -16,10 +16,11 @@ local function override_terminal_open()
 			local cfg_dir = vim.fn.getenv("HOME") .. "/.config/lazygit"
 
 			opts = vim.tbl_deep_extend("force", opts or {}, {
+				backdrop = 70,
 				border = "none",
 				env = { ["LG_CONFIG_FILE"] = cfg_dir .. "/config.yml," .. cfg_dir .. "/theme-" .. theme .. ".yml" },
 				size = { height = 1, width = 1 },
-				margin = { top = 1, bottom = 1 },
+				margin = { top = 1, bottom = 0 },
 			})
 		end
 
