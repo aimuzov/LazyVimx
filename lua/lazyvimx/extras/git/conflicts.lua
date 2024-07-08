9be2ce9ce37c8c5926899b9759fa9bb5fddf5f1c
Aleksey Imuzov <github@aimuzov.online>
2024-07-08 11:54:56 +0400

    feat(git): improve notifications from git-conflict plugin

--- lua/lazyvimx/extras/git/conflicts.lua
+++ lua/lazyvimx/extras/git/conflicts.lua
@@ -1,14 +1,29 @@
+local interval = 3000
+
+local function notifier_create(text, level)
+	return function()
+		vim.notify("Conflict " .. text .. " in '" .. vim.fn.expand("%:.") .. "'", level)
+	end
+end
+
 return {
 	"akinsho/git-conflict.nvim",
 
 	opts = function(_, opts)
+		local throttle = require("git-conflict.utils").throttle
+		local notifier_detected = throttle(interval, notifier_create("detected", vim.log.levels.WARN))
+		local notifier_resolved = throttle(interval, notifier_create("resolved", vim.log.levels.INFO))
+
 		opts.disable_diagnostics = true
 
 		vim.api.nvim_create_autocmd("User", {
 			pattern = "GitConflictDetected",
-			callback = function()
-				vim.notify("Conflict detected in '" .. vim.fn.expand("%:.") .. "'")
-			end,
+			callback = notifier_detected,
+		})
+
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "GitConflictResolved",
+			callback = notifier_resolved,
 		})
 	end,
 }
