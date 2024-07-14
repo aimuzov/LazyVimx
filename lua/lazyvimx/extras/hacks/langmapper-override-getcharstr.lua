cff9b2ce37c32c0154817928beb360e9f201c5f8
Aleksey Imuzov <github@aimuzov.online>
2024-07-14 16:31:50 +0400

    fix(nav): add missing arg for getcharstr func

--- lua/lazyvimx/extras/hacks/langmapper-override-getcharstr.lua
+++ lua/lazyvimx/extras/hacks/langmapper-override-getcharstr.lua
@@ -7,8 +7,8 @@ return {
 		opts = function()
 			local getcharstr_orig = vim.fn.getcharstr
 
-			vim.fn.getcharstr = function()
-				local char = getcharstr_orig()
+			vim.fn.getcharstr = function(expr)
+				local char = getcharstr_orig(expr)
 				local char_translated = require("langmapper.utils").translate_keycode(char, "default", "ru")
 
 				return char_translated
