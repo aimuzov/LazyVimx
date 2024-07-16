29891ae43ae8908728437ef4f71d42af4b45fc05
Aleksey Imuzov <github@aimuzov.online>
2024-07-16 12:16:44 +0400

    chore(hacks): small fix

--- lua/lazyvimx/util/hacks.lua
+++ lua/lazyvimx/util/hacks.lua
@@ -1,7 +1,7 @@
 local M = {}
 
-function M.getcharstr(expr)
-	local char = vim.fn.getcharstr(expr)
+function M.getcharstr()
+	local char = vim.fn.getcharstr()
 
 	if LazyVim.has("langmapper.nvim") and char ~= "" then
 		char = require("langmapper.utils").translate_keycode(char, "default", "ru")
