59717ba8338b53df7526365191248b888401ef88
Aleksey Imuzov <github@aimuzov.online>
2024-07-30 09:30:28 +0400

    refactor(nav): remove useless table

--- lua/lazyvimx/extras/ui/panels/key-helper.lua
+++ lua/lazyvimx/extras/ui/panels/key-helper.lua
@@ -1,10 +1,8 @@
 return {
-	{
-		"folke/which-key.nvim",
-		opts = {
-			layout = { spacing = 5 },
-			plugins = { spelling = { enabled = false } },
-			win = { wo = { winblend = 15 } },
-		},
+	"folke/which-key.nvim",
+	opts = {
+		layout = { spacing = 5 },
+		plugins = { spelling = { enabled = false } },
+		win = { wo = { winblend = 15 } },
 	},
 }
