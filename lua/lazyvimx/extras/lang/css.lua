eadaf4e41bea1e0a54e1f1186d7f77de884d6323
Aleksey Imuzov <github@aimuzov.online>
2024-09-05 13:23:48 +0400

    fix(lang): add scss treesitter

--- lua/lazyvimx/extras/lang/css.lua
+++ lua/lazyvimx/extras/lang/css.lua
@@ -20,7 +20,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		opts = function(_, opts)
 			opts.ensure_installed = opts.ensure_installed or {}
-			vim.list_extend(opts.ensure_installed, { "css" })
+			vim.list_extend(opts.ensure_installed, { "css", "scss" })
 		end,
 	},
 
