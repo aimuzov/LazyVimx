54683a8bbf91a4c4f9e7934fe92e5c56eab626f0
Aleksey Imuzov <github@aimuzov.online>
2024-09-06 09:03:23 +0400

    refactor(ui): remove useless table

--- lua/lazyvimx/extras/ui/style/popups/size.lua
+++ lua/lazyvimx/extras/ui/style/popups/size.lua
@@ -1,7 +1,5 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		optional = true,
-		opts = { ui = { width = 0.6, height = 0.7 } },
-	},
+	"williamboman/mason.nvim",
+	optional = true,
+	opts = { ui = { width = 0.6, height = 0.7 } },
 }
