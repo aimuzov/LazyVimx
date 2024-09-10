b51293a99d412f336d61478f6205fc7054d68884
Aleksey Imuzov <github@aimuzov.online>
2024-09-10 16:41:00 +0400

    refactor(linting): fix config style

--- lua/lazyvimx/extras/linting/cspell.lua
+++ lua/lazyvimx/extras/linting/cspell.lua
@@ -1,14 +1,9 @@
 return {
-	{
-		"mfussenegger/nvim-lint",
-
-		opts = function(_, opts)
-			if vim.fn.executable("cspell") == 0 then
-				return
-			end
-
+	"mfussenegger/nvim-lint",
+	opts = function(_, opts)
+		if vim.fn.executable("cspell") == 1 then
 			opts.linters_by_ft["*"] = opts.linters_by_ft["*"] or {}
 			table.insert(opts.linters_by_ft["*"], "cspell")
-		end,
-	},
+		end
+	end,
 }
