6a7c7c45ab01aa1522ce7f159ae4c733bf72ba1b
Aleksey Imuzov <github@aimuzov.online>
2024-10-03 09:54:59 +0400

    chore(linting): remove some useless config for stylelint

--- lua/lazyvimx/extras/linting/stylelint.lua
+++ lua/lazyvimx/extras/linting/stylelint.lua
@@ -1,14 +1,4 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		optional = true,
-		opts = function(_, opts)
-			require("lspconfig.server_configurations.stylelint_lsp").default_config.filetypes = {}
-		end,
-	},
-
-	{
-		"williamboman/mason.nvim",
-		opts = { ensure_installed = { "stylelint" } },
-	},
+	"williamboman/mason.nvim",
+	opts = { ensure_installed = { "stylelint" } },
 }
