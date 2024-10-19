6c19fc5d9fe7f7cab4247231e1d374c177a41e79
Aleksey Imuzov <github@aimuzov.online>
2024-10-19 11:23:47 +0400

    feat(nav): add some integrations for mupltiple cursors

--- lua/lazyvimx/extras/nav/multiple-cursors.lua
+++ lua/lazyvimx/extras/nav/multiple-cursors.lua
@@ -1,17 +1,96 @@
 return {
-	"brenton-leighton/multiple-cursors.nvim",
+	{
+		"brenton-leighton/multiple-cursors.nvim",
+		dependencies = { "hrsh7th/nvim-cmp" },
 
-	opts = {
-		pre_hook = function()
-			vim.opt.cursorline = false
-			vim.g.minipairs_disable = true
-			vim.cmd("NoMatchParen")
-		end,
+		opts = {
+			pre_hook = function()
+				vim.opt.cursorline = false
+				vim.g.minipairs_disable = true
 
-		post_hook = function()
-			vim.opt.cursorline = true
-			vim.g.minipairs_disable = false
-			vim.cmd("DoMatchParen")
+				vim.cmd("NoMatchParen")
+
+				require("cmp").setup({ enabled = false })
+			end,
+
+			post_hook = function()
+				vim.opt.cursorline = true
+				vim.g.minipairs_disable = false
+
+				vim.cmd("DoMatchParen")
+
+				require("cmp").setup({ enabled = true })
+			end,
+		},
+	},
+
+	{
+		"folke/which-key.nvim",
+		optional = true,
+		opts = function()
+			require("which-key.plugins.presets").operators["v"] = nil
 		end,
+	},
+
+	{
+		"gbprod/stay-in-place.nvim",
+		optional = true,
+
+		dependencies = {
+			"brenton-leighton/multiple-cursors.nvim",
+			opts = {
+				custom_key_maps = {
+				-- stylua: ignore start
+				{ "n", { ">>", "<Tab>" },  function() require("stay-in-place").shift_right_line() end, },
+				{ "n", "<<",               function() require("stay-in-place").shift_left_line() end, },
+				{ { "n", "i" }, "<S-Tab>", function() require("stay-in-place").shift_left_line() end, },
+					-- stylua: ignore end
+				},
+			},
+		},
+	},
+
+	{
+		"chrisgrieser/nvim-spider",
+		optional = true,
+		dependencies = {
+			"brenton-leighton/multiple-cursors.nvim",
+			opts = {
+				custom_key_maps = {
+					{
+						{ "n", "x" },
+						"w",
+						function(_, count)
+							if count ~= 0 and vim.api.nvim_get_mode().mode == "n" then
+								vim.cmd("normal! " .. count)
+							end
+							require("spider").motion("w")
+						end,
+					},
+
+					{
+						{ "n", "x" },
+						"e",
+						function(_, count)
+							if count ~= 0 and vim.api.nvim_get_mode().mode == "n" then
+								vim.cmd("normal! " .. count)
+							end
+							require("spider").motion("e")
+						end,
+					},
+
+					{
+						{ "n", "x" },
+						"b",
+						function(_, count)
+							if count ~= 0 and vim.api.nvim_get_mode().mode == "n" then
+								vim.cmd("normal! " .. count)
+							end
+							require("spider").motion("b")
+						end,
+					},
+				},
+			},
+		},
 	},
 }
