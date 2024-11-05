ffabd4742b238d69eedee8e309fe74fdab0ad8fd
Aleksey Imuzov <github@aimuzov.online>
2024-11-05 13:26:42 +0400

    feat(ui): improve style (usage helpers)

--- lua/lazyvimx/extras/ui/editor/line-hints/symbol-usage.lua
+++ lua/lazyvimx/extras/ui/editor/line-hints/symbol-usage.lua
@@ -1,14 +1,53 @@
+function text_format(symbol)
+	local res = {}
+
+	local stacked_functions_content = symbol.stacked_count > 0 and ("+%s"):format(symbol.stacked_count) or ""
+
+	if symbol.references then
+		local usage = symbol.references <= 1 and "usage" or "usages"
+		local num = symbol.references == 0 and "no" or symbol.references
+
+		table.insert(res, { "󰌹 ", "SymbolUsageRef" })
+		table.insert(res, { ("%s %s"):format(num, usage), "SymbolUsageContent" })
+	end
+
+	if symbol.definition then
+		if #res > 0 then
+			table.insert(res, { " ", "NonText" })
+		end
+
+		table.insert(res, { "󰳽 ", "SymbolUsageDef" })
+		table.insert(res, { symbol.definition .. " defs", "SymbolUsageContent" })
+	end
+
+	if symbol.implementation then
+		if #res > 0 then
+			table.insert(res, { " ", "NonText" })
+		end
+
+		table.insert(res, { "󰡱 ", "SymbolUsageImpl" })
+		table.insert(res, { symbol.implementation .. " impls", "SymbolUsageContent" })
+	end
+
+	if stacked_functions_content ~= "" then
+		if #res > 0 then
+			table.insert(res, { " ", "NonText" })
+		end
+
+		table.insert(res, { " ", "SymbolUsageImpl" })
+		table.insert(res, { stacked_functions_content, "SymbolUsageContent" })
+	end
+
+	return res
+end
+
 return {
 	"Wansmer/symbol-usage.nvim",
 	event = "BufReadPre",
-	opts = function(_, opts)
-		opts.vt_position = "end_of_line"
-		opts.request_pending_text = "symbol"
-		opts.hl = { link = "GitSignsCurrentLineBlame" }
-		opts.text_format = function(symbol)
-			local text = require("symbol-usage.options")._default_opts.text_format(symbol)
-
-			return "󰌹 " .. text
-		end
-	end,
+	opts = {
+		vt_position = "end_of_line",
+		definition = { enabled = false },
+		implementation = { enabled = false },
+		text_format = text_format,
+	},
 }
