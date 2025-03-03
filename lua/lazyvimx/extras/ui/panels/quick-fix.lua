aed5b81058ff92d0249ef17b745686a8f1c60af1
Aleksey Imuzov <github@aimuzov.online>
2025-03-03 13:40:41 +0400

    fix(ui): fix quickfix replacing by trouble

--- lua/lazyvimx/extras/ui/panels/quick-fix.lua
+++ lua/lazyvimx/extras/ui/panels/quick-fix.lua
@@ -1,9 +1,22 @@
 return {
 	"folke/trouble.nvim",
 	opts = function()
-		vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+		vim.api.nvim_create_autocmd("BufWinEnter", {
+			pattern = "quickfix",
 			callback = function()
-				vim.cmd([[Trouble qflist open]])
+				local trouble = require("trouble")
+
+				if vim.fn.getloclist(0, { filewinid = 1 }).filewinid ~= 0 then
+					vim.defer_fn(function()
+						vim.cmd.lclose()
+						trouble.open("loclist")
+					end, 0)
+				else
+					vim.defer_fn(function()
+						vim.cmd.cclose()
+						trouble.open("quickfix")
+					end, 0)
+				end
 			end,
 		})
 	end,
