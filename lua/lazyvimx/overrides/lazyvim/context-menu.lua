731b31300bbbb855af59926a6f9d7de78d405305
Aleksey Imuzov <github@aimuzov.online>
2025-04-21 21:59:29 +0400

    fix(overrides): move mouse setup to overrides
    Closes #14

--- lua/lazyvimx/overrides/lazyvim/context-menu.lua
+++ lua/lazyvimx/overrides/lazyvim/context-menu.lua
@@ -3,5 +3,7 @@ return {
 	opts = function()
 		vim.cmd.aunmenu({ "PopUp.How-to\\ disable\\ mouse" })
 		vim.cmd.aunmenu({ "PopUp.-1-" })
+
+		vim.cmd.amenu({ "PopUp.Close <cmd>:close<cr>" })
 	end,
 }
