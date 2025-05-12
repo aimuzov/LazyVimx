1a067067e989033764c5acc34479db7a8affe70c
Aleksey Imuzov <github@aimuzov.online>
2025-05-12 10:38:02 +0400

    fix(vscode): enable some plugins for motions

--- lua/lazyvimx/extras/motions/better-move-between-words.lua
+++ lua/lazyvimx/extras/motions/better-move-between-words.lua
@@ -3,12 +3,14 @@ return {
 		"chrisgrieser/nvim-spider",
 		desc = "Use the w, e, b motions like a spider. Move by subwords and skip insignificant punctuation",
 		dependencies = { "vhyrro/luarocks.nvim" },
+		vscode = true,
 		config = true,
 	},
 
 	{
 		"vhyrro/luarocks.nvim",
 		priority = 1000,
+		vscode = true,
 		opts = { rocks = { "luautf8" } },
 	},
 }
