3caf5764c1f6abfd290e01a62a11fa83e67fe2dd
Aleksey Imuzov <github@aimuzov.online>
2025-05-14 13:54:22 +0400

    fix(boot): fix overriding vim.g.root_lsp_ignore

--- lua/lazyvimx/boot.lua
+++ lua/lazyvimx/boot.lua
@@ -12,17 +12,26 @@ end
 
 local function set_global()
 	vim.g.lazyvim_check_order = false
-	vim.g.root_lsp_ignore = { "eslint" }
 	vim.g.xtras_prios = {}
 	vim.g.lazyvim_explorer = "neo-tree"
 
 	return false
 end
 
+local function update_root_lsp_ignore()
+	vim.g.root_lsp_ignore = {
+		unpack(vim.g.root_lsp_ignore),
+		"eslint",
+	}
+end
+
 return {
-	{ "aimuzov/LazyVimx", dependencies = { "LazyVim/LazyVim" }, vscode = true, config = true },
 	{ import = "system.plug", enabled = set_global },
+
+	{ "LazyVim/LazyVim", opts = update_root_lsp_ignore },
 	{ "LazyVim/LazyVim", opts = insert_extras },
 	{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
 	{ "LazyVim/LazyVim", opts = set_colorscheme },
+
+	{ "aimuzov/LazyVimx", dependencies = { "LazyVim/LazyVim" }, vscode = true, config = true },
 }
